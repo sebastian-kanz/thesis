f895b82365b161d7afe9c68fee23493ee4a897e0
Sebastian Kanz <sebastiankanz@t-online.de>
2019-09-30 16:20:48 +0200

    Ongoing work on proposal

--- proposal/latexdockercmd2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-IMAGE=blang/latex:ctanfull
-#exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" latexmk -C
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex "$@"
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" bibtex "$@"
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex "$@"
-#docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex "$@"
-#docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" latexmk -c
