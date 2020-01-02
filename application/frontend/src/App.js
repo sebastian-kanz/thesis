6a7100af12ef334c33555c612d0276e9a434fc91
Sebastian Kanz <sebastian.kanz@maibornwolff.de>
2020-01-02 17:35:06 +0100

    work chapter 1 to 6 - unclean

---
+++ application/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './components/layout/Test';
+import ButtonAppBar from './components/layout/ButtonAppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar/>
+    </div>
+  );
+}
+
+export default App;
